28e3df0ec6c1882264da52ab53800d9ea055eaf1
Рыжов Виктор <ryzhov.dur@gmail.com>
2020-02-16 20:14:47 +0300

    Lesson_003 Task_06

--- lesson_003/06_rainbow.py
+++ lesson_003/06_rainbow.py
@@ -10,22 +10,13 @@ rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN
 sd.resolution = (1200, 600)
 
 # Нарисовать радугу: 7 линий разного цвета толщиной 4 с шагом 5 из точки (50, 50) в точку (350, 450)
-start_point = sd.get_point(50, 50)
-end_point = sd.get_point(350, 450)
-
-# TODO Лучше пока не использовать функцию, либо доделывать её до конца
-# TODO Чтобы она использовала точку, которую ей передают параметром, а не переменную start_point/end_point
-# TODO Которые расположены "снаружи" функции.
-def rainbow(line, step):  # TODO Тут в параметры надо будет добавить два параметра
-    sd.line(start_point=start_point, end_point=end_point, color=sd.COLOR_BLACK, width=4)
-    # TODO И тут в sd.line использовать эти параметры вместо начальной и конечной точки
-    line += step
-
-
-for x in range(100, 701, 100):
-    start_point = sd.get_point(x, 50)
-    end_point = sd.get_point(x, 450)
-    rainbow(line=100, step=5)  # TODO Здесь на место этих параметров надо будет передавать start и end point-s
+step = 0
+for line in range(50, 350, 450):
+    line_1 = line + 5
+    step += 5
+    start_point = sd.get_point(50, 50)
+    end_point = sd.get_point(350, 450)
+    sd.line(start_point, end_point, color=sd.COLOR_RED, width=4)
 
 # Усложненное задание, делать по желанию.
 # Нарисовать радугу дугами от окружности (cсм sd.circle) за нижним краем экрана,
