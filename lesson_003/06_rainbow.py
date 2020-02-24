3a275f306455c2d9657b9a6752e583481441d3f3
Виктор Рыжов <ryzhov.dur@gmail.com>
2020-02-25 01:08:57 +0300

    lessons_03  _fixed_ task_06

--- lesson_003/06_rainbow.py
+++ lesson_003/06_rainbow.py
@@ -10,12 +10,12 @@ rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN
 sd.resolution = (1200, 600)
 
 # Нарисовать радугу: 7 линий разного цвета толщиной 4 с шагом 5 из точки (50, 50) в точку (350, 450)
-
-start_point = sd.get_point(50, 50)
-end_point = sd.get_point(350, 450)
+x = 0
 for colors in rainbow_colors:
+    x += 5
+    start_point = sd.get_point(50 + x, 50)
+    end_point = sd.get_point(350 + x, 450)
     sd.line(start_point, end_point, color=colors, width=4)
-# TODO Не понимаю как сдвинуть точку. Проще было усложненное задание сделать!
 
 # Усложненное задание, делать по желанию.
 # Нарисовать радугу дугами от окружности (cсм sd.circle) за нижним краем экрана,
