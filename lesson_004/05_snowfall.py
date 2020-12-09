1a53389d010e58e0a2928feb5afe1df9dc48c836
Victor <ryzhov.dur@gmail.com>
2020-12-09 16:12:42 +0300

    Lessons_004 Task_05 Проверьте пожалуйста. Что-то мне кажется, что не очень правильно

--- lesson_004/05_snowfall.py
+++ lesson_004/05_snowfall.py
@@ -4,6 +4,7 @@ import simple_draw as sd
 
 sd.resolution = (1200, 600)
 
+
 # На основе кода из практической части реализовать снегопад:
 # - создать списки данных для отрисовки N снежинок
 # - нарисовать падение этих N снежинок
@@ -21,38 +22,39 @@ sd.resolution = (1200, 600)
 
 def snowfall(N):
     snowflake = []
+    sd.start_drawing()
     for _ in range(N):
         snowflake.append(([sd.random_number(-50, 1250), sd.random_number(550, 1600), sd.random_number(23, 47)]))
 
     # TODO цикл while должен начинаться после цикла for, а не внутри него
-        while True:
-            sd.start_drawing()
-            # TODO start - в начале, перед циклом for
-            # TODO finish - в конце, перед sd.sleep
-            # TODO Сами по себе эти функции нужны, чтобы не рисовать каждое промежуточное действие по-отдельности
-            # TODO start останавливает рисование, finish запускает рисование всего, что накопилось с момента start-а
-            for snow in snowflake:
-                x, y, length = snow
-                point = sd.get_point(x, y)
+    while True:
+        # TODO start - в начале, перед циклом for
+        # TODO finish - в конце, перед sd.sleep
+        # TODO Сами по себе эти функции нужны, чтобы не рисовать каждое промежуточное действие по-отдельности
+        # TODO start останавливает рисование, finish запускает рисование всего, что накопилось с момента start-а
+        for snow in snowflake:
+            x, y, length = snow
+            point = sd.get_point(x, y)
 
-                sd.snowflake(center=point, length=length, color=sd.background_color, factor_a=0.6)
-                if y > 50:
-                    snow[1] -= 10
-                    x -= sd.random_number(-25, 25)  # TODO это изменение тоже надо производить по индексу
-                    # TODO snow[0] -= ...
-                    # TODO иначе оно не будет сохранено в списке, т.к изменяться будет только переменная x
-                    point_fall = sd.get_point(x, y)
-                    sd.snowflake(point_fall, length=length, color=sd.COLOR_WHITE)
-                else:
-                    last_point = sd.get_point(x, y - 1)
-                    sd.snowflake(last_point, length, color=sd.COLOR_WHITE)
-                    y += sd.random_number(600, 800)  # TODO и тут, snow[1] +=
+            sd.snowflake(center=point, length=length, color=sd.background_color, factor_a=0.6)
+            if y > 50:
+                snow[1] -= 10
+                x -= sd.random_number(-25, 25)  # TODO это изменение тоже надо производить по индексу
+                snow[0] -= 10
+                # TODO иначе оно не будет сохранено в списке, т.к изменяться будет только переменная x
+                point_fall = sd.get_point(x, y)
+                sd.snowflake(point_fall, length=length, color=sd.COLOR_WHITE)
+            else:
+                last_point = sd.get_point(x, y - 1)
+                sd.snowflake(last_point, length, color=sd.COLOR_WHITE)
+                y += sd.random_number(600, 800)  # TODO и тут, snow[1] +=
+                snow[1] += 50
 
-            sd.finish_drawing()
-            sd.sleep(0.01)
-            if sd.user_want_exit():
-                break
-            # sd.clear_screen()
+        sd.finish_drawing()
+        sd.sleep(0.05)
+        if sd.user_want_exit():
+            break
+        sd.clear_screen()
 
 
 snowfall(20)
