560f2cf3da1e42637cafebcac2bb1789dbfe6336
Victor <ryzhov.dur@gmail.com>
2021-01-08 00:28:03 +0300

    Lessons_005 Task_01 Проверьте пожалуйста, и можно ли приступать к следующему заданию?

--- lesson_005/01_inhabitants.py
+++ lesson_005/01_inhabitants.py
@@ -3,4 +3,8 @@
 # Вывести на консоль жителей комнат (модули room_1 и room_2)
 # Формат: В комнате room_1 живут: ...
 
-# TODO здесь ваш код
+from room_1 import folks as r1
+from room_2 import folks as r2
+
+print('В комнате room_1 живут:', r1[0], 'и', r1[1])
+print('В комнате room_2 живет:', r2[0])
