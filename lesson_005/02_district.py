f828a3fb3a800a30f29bc3449d2612662da569b6
Victor <ryzhov.dur@gmail.com>
2021-01-10 00:49:07 +0300

    Lessons_005 Task_02 Проверьте пожалуйста, и можно ли приступать к следующему заданию?

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -5,4 +5,18 @@
 # подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
-# TODO здесь ваш код
+from pprint import pprint
+from district.central_street.house1.room1 import folks as central_street_h1_r1
+from district.central_street.house1.room2 import folks as central_street_h1_r2
+from district.central_street.house2.room1 import folks as central_street_h2_r1
+from district.central_street.house2.room2 import folks as central_street_h2_r2
+from district.soviet_street.house1.room1 import folks as soviet_street_h1_r1
+from district.soviet_street.house1.room2 import folks as soviet_street_h1_r2
+from district.soviet_street.house2.room1 import folks as soviet_street_h2_r1
+from district.soviet_street.house2.room2 import folks as soviet_street_h2_r2
+
+residents = [', '.join(central_street_h1_r1), ', '.join(central_street_h1_r2), ', '.join(central_street_h2_r1),
+             ', '.join(central_street_h2_r2), ', '.join(soviet_street_h1_r1), ', '.join(soviet_street_h1_r2),
+             ', '.join(soviet_street_h2_r1), ', '.join(soviet_street_h2_r2)]
+
+pprint("На районе живут: " + ', '.join(map(str, residents)))
