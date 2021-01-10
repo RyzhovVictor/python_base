9a59e3c43ffa01079323e2534623181eadf89c26
Victor <ryzhov.dur@gmail.com>
2021-01-10 19:05:18 +0300

    Lessons_005 Task_01 Проверьте пожалуйста, и можно ли приступать к следующему заданию?

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -15,10 +15,9 @@ from district.soviet_street.house1.room2 import folks as soviet_street_h1_r2
 from district.soviet_street.house2.room1 import folks as soviet_street_h2_r1
 from district.soviet_street.house2.room2 import folks as soviet_street_h2_r2
 
-# TODO попробуйте сперва сложить все списки (+)
-# TODO а затем один join использовать
-residents = [', '.join(central_street_h1_r1), ', '.join(central_street_h1_r2), ', '.join(central_street_h2_r1),
-             ', '.join(central_street_h2_r2), ', '.join(soviet_street_h1_r1), ', '.join(soviet_street_h1_r2),
-             ', '.join(soviet_street_h2_r1), ', '.join(soviet_street_h2_r2)]
+
+residents = [', '.join(central_street_h1_r1 + central_street_h1_r2 + central_street_h2_r1 +
+                       central_street_h2_r2 + soviet_street_h1_r1 + soviet_street_h1_r2 +
+                       soviet_street_h2_r1 + soviet_street_h2_r2)]
 
 pprint("На районе живут: " + ', '.join(map(str, residents)))
