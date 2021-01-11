51791e446276f9ad6a3bfe0f5db7f749bd4c2157
Максим <bi4ok23@gmail.com>
2021-01-11 12:43:17 +0300

    lesson 005: нужны правки в 02, 03, 04

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -16,8 +16,10 @@ from district.soviet_street.house2.room1 import folks as soviet_street_h2_r1
 from district.soviet_street.house2.room2 import folks as soviet_street_h2_r2
 
 
+# TODO внешний список (скобки []) - можно убрать
 residents = [', '.join(central_street_h1_r1 + central_street_h1_r2 + central_street_h2_r1 +
                        central_street_h2_r2 + soviet_street_h1_r1 + soviet_street_h1_r2 +
                        soviet_street_h2_r1 + soviet_street_h2_r2)]
 
+# TODO тогда тут не нужен будет ни джойн, ни мэп, можно будет просто распечатать residents
 pprint("На районе живут: " + ', '.join(map(str, residents)))
