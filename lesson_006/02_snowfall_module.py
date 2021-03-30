0d10e1011aead21bc0cdfe6d2ce3fa8a3188984e
Victor <ryzhov.dur@gmail.com>
2021-03-30 18:51:55 +0300

    Lessons_006 Task_02 Подправил пока сам код, посидел подумал и вроде что-то вышло проверьте

--- lesson_006/02_snowfall_module.py
+++ lesson_006/02_snowfall_module.py
@@ -6,8 +6,6 @@ from snowfall import draw_snowflakes
 from snowfall import move_snowflakes
 from snowfall import number_snowflakes
 from snowfall import delete_snowflakes
-from snowfall import flakes
-
 
 sd.resolution = (1200, 600)
 
@@ -23,7 +21,9 @@ sd.resolution = (1200, 600)
 # В текущем модуле реализовать главный цикл падения снежинок,
 # обращаясь ТОЛЬКО к функциям модуля snowfall
 
-create_snowflakes(20)
+create_snowflakes(50)
+color = sd.background_color
+color_1 = sd.COLOR_YELLOW
 while True:
     #  нарисовать_снежинки_цветом(color=sd.background_color)
     #  сдвинуть_снежинки()
@@ -32,21 +32,18 @@ while True:
     #       удалить_снежинки(номера)
     #       создать_снежинки(count)
     sd.start_drawing()
-    for flake in flakes:  # TODO здесь не нужен цикл по снежинкам
-        # TODO в draw_snowflakes и других функциях уже зашиты свои циклы
-        # TODO тут нужно просто вызывать эти функции в правильном порядке
-        draw_snowflakes(sd.background_color)
-        move_snowflakes()
-        draw_snowflakes(sd.COLOR_YELLOW)
-        draw_snowflakes(sd.background_color)
-        fallen_flakes = number_snowflakes()
-        delete_snowflakes(number=fallen_flakes)
-    sd.finish_drawing()
-    sd.sleep(0.1)
-    if sd.user_want_exit():  # TODO дублирование кода надо убрать
-        break
-    sd.sleep(0.1)
-    if sd.user_want_exit():
-        break
+    draw_snowflakes(color)
+    move_snowflakes()
+    draw_snowflakes(color_1)
+    # fallen_flakes = number_snowflakes()
+    if number_snowflakes():
+        delete_snowflakes(number=number_snowflakes())
+        create_snowflakes(50)
+    else:
+        sd.finish_drawing()
+        sd.sleep(0.05)
+        if sd.user_want_exit():
+            break
+
 
 sd.pause()
--- lesson_006/snowfall.py
+++ lesson_006/snowfall.py
@@ -7,7 +7,7 @@ def create_snowflakes(n):
     global snowflakes
     snowflakes = []
     for i in range(0, n):
-        snowflakes.append(([sd.random_number(-50, 1250), sd.random_number(550, 1600), sd.random_number(23, 47)]))
+        snowflakes.append(([sd.random_number(-50, 1400), sd.random_number(700, 1600), sd.random_number(23, 47)]))
     return snowflakes
 
 
@@ -21,8 +21,8 @@ def draw_snowflakes(color):
 def move_snowflakes():
     for coordinate in snowflakes:
         if coordinate[1] > 50:
-            coordinate[1] -= 10
-            coordinate[0] -= 10
+            coordinate[1] -= 20
+            coordinate[0] -= 5
             point_fall = sd.get_point(coordinate[0], coordinate[1])
             sd.snowflake(point_fall, length=coordinate[2], color=sd.COLOR_WHITE)
         else:
@@ -31,16 +31,12 @@ def move_snowflakes():
             coordinate[1] += 1250
 
 
-flakes = create_snowflakes(20)  # TODO создавать снежинки тут не нужно
-# TODO достаточно инициализировать пустой список в НАЧАЛЕ файла
-
-
 def number_snowflakes():
     global new_snowflakes
     new_snowflakes = []
     for index, flake in enumerate(snowflakes):
         if snowflakes[1] <= snowflakes[2]:
-            new_snowflakes.append(flake)  # TODO добавлять тут нужно индекс, а не flake
+            new_snowflakes.append(index)
     return new_snowflakes
 
 
