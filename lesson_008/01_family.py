0ce20b1cb28607ce065f291c053d936f22b91527
Максим <bi4ok23@gmail.com>
2021-04-14 10:56:58 +0300

    lesson 008: зачёт

--- lesson_008/01_family.py
+++ lesson_008/01_family.py
@@ -395,3 +395,4 @@ cprint('Всего купленно шуб: {}'.format(count_coat), color='yello
 #       for salary in range(50, 401, 50):
 #           max_cats = life.experiment(salary)
 #           print(f'При зарплате {salary} максимально можно прокормить {max_cats} котов')
+#зачёт!
