a5fbef8e24baa6d8da0a780eaede8a3d3b9db4af
Victor <ryzhov.dur@gmail.com>
2021-04-20 19:46:08 +0300

    Lessons_009 Task_01 Последние изменения данного задания. Не выполненно сортировка.

--- lesson_009/01_char_stat.py
+++ lesson_009/01_char_stat.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import zipfile
+from random import randint
+
 # Подсчитать статистику по буквам в романе Война и Мир.
 # Входные параметры: файл для сканирования
 # Статистику считать только для букв алфавита (см функцию .isalpha() для строк)
@@ -25,7 +28,49 @@
 #   см https://refactoring.guru/ru/design-patterns/template-method
 #   и https://gitlab.skillbox.ru/vadim_shandrinov/python_base_snippets/snippets/4
 
-# TODO здесь ваш код
+file_name = 'voyna-i-mir.txt'
+stat = {}
+
+with open(file_name, 'r', encoding='cp1251') as file:
+    for line in file:
+        for prev_char in line:
+            if prev_char.isalpha():
+                if prev_char in stat:
+                    stat[prev_char] += 1
+                else:
+                    stat[prev_char] = 1
+
+txt = '+'
+print(f'+{txt:-^30}+')
+
+txt = '|'
+txt_1 = 'Буква'
+txt_2 = 'Частота'
+print(f'|{txt_1:^13} {txt:^1} {txt_2:^14}|')
+
+txt = '+'
+print(f'+{txt:-^30}+')
+
+total_count = 0
+for alphabet, count in stat.items():
+    txt = '|'
+    print(f'|{alphabet:^13} {txt:^1} {count:^14}|')
+    total_count += count
+    # for sort in sorted(str(count)):
+    #     txt = '|'
+    #     print(f'|{alphabet:^13} {txt:^1} {count:^14}|')
+    #     total_count += count
+
+txt = '+'
+print(f'+{txt:-^30}+')
+
+txt = '|'
+txt_1 = 'ИТОГО'
+txt_2 = total_count
+print(f'|{txt_1:^13} {txt:^1} {txt_2:^14}|')
+txt = '+'
+print(f'+{txt:-^30}+')
+
 
 # После зачета первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
