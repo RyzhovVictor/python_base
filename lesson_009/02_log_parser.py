2a84e97a827d3afd6eca9ed97e18dd59649da5ac
Victor <ryzhov.dur@gmail.com>
2021-08-23 20:33:52 +0300

    Lessons_009 Task_02 Доработка

--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -23,7 +23,29 @@
 #   см https://refactoring.guru/ru/design-patterns/template-method
 #   и https://gitlab.skillbox.ru/vadim_shandrinov/python_base_snippets/snippets/4
 
-# TODO здесь ваш код
+from datetime import datetime as dt
+
+
+class Events:
+    def __init__(self, input_file, output_file):
+        self.input_file = input_file
+        self.output_file = output_file
+        count = 0
+
+    def read(self):
+        with open(self.input_file, 'r', encoding='utf-8') as file:
+            for line in file:
+                if 'NOK' in line:
+                    line = line.strip()[1:17]
+                    print(line)
+
+    # def write(self, add):
+    #     with open(self.output_file, 'a', encoding='utf-8') as file:
+    #         file.write(add)
+
+
+read = Events('events.txt', 'output_file.txt')
+read.read()
 
 # После зачета первого этапа нужно сделать группировку событий
 #  - по часам
