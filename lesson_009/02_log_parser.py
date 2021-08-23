967330c6dc5be30e790ca209d7dadd284cd5136f
Рыжов Виктор <ryzhov.dur@gmail.com>
2021-08-23 21:12:41 +0300

    Lessons_09 Task_02 Нужна помощь

--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -23,14 +23,12 @@
 #   см https://refactoring.guru/ru/design-patterns/template-method
 #   и https://gitlab.skillbox.ru/vadim_shandrinov/python_base_snippets/snippets/4
 
-from datetime import datetime as dt
-
 
 class Events:
     def __init__(self, input_file, output_file):
         self.input_file = input_file
         self.output_file = output_file
-        count = 0
+        self.count = 0
 
     def read(self):
         with open(self.input_file, 'r', encoding='utf-8') as file:
@@ -39,9 +37,6 @@ class Events:
                     line = line.strip()[1:17]
                     print(line)
 
-    # def write(self, add):
-    #     with open(self.output_file, 'a', encoding='utf-8') as file:
-    #         file.write(add)
 
 
 read = Events('events.txt', 'output_file.txt')
