057d72e93dcd5758c82efe197878de1d25eb4624
Victor <ryzhov.dur@gmail.com>
2021-08-24 15:31:31 +0300

    Lessons_009 Task_02 Вторая часть

--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -30,7 +30,7 @@ class Events:
         self.output_file = output_file
         self.stat = {}
 
-    def read(self):
+    def read_minute(self):
         with open(self.input_file, 'r', encoding='utf-8') as file:
             for line in file:
                 if 'NOK' in line:
@@ -40,16 +40,54 @@ class Events:
                     else:
                         self.stat[line] = 1
 
+    def read_hours(self):
+        with open(self.input_file, 'r', encoding='utf-8') as file:
+            for line in file:
+                if 'NOK' in line:
+                    line = line.strip()[1:14]
+                    if line in self.stat:
+                        self.stat[line] += 1
+                    else:
+                        self.stat[line] = 1
+
+    def read_month(self):
+        with open(self.input_file, 'r', encoding='utf-8') as file:
+            for line in file:
+                if 'NOK' in line:
+                    line = line.strip()[1:8]
+                    if line in self.stat:
+                        self.stat[line] += 1
+                    else:
+                        self.stat[line] = 1
+
+    def read_years(self):
+        with open(self.input_file, 'r', encoding='utf-8') as file:
+            for line in file:
+                if 'NOK' in line:
+                    line = line.strip()[1:5]
+                    if line in self.stat:
+                        self.stat[line] += 1
+                    else:
+                        self.stat[line] = 1
+
     def write(self):
         with open(self.output_file, 'w', encoding='utf-8') as file:
             for dates, count in self.stat.items():
-                # print(f'[{dates}] {count}')
                 file.write(f'[{dates}] {count}\n')
 
 
-processing = Events('events.txt', 'output_file.txt')
-processing.read()
-processing.write()
+processing_minute = Events('events.txt', 'sort_minute.txt')
+processing_minute.read_minute()
+processing_hours = Events('events.txt', 'sort_hours.txt')
+processing_hours.read_hours()
+processing_month = Events('events.txt', 'sort_month.txt')
+processing_month.read_month()
+processing_years = Events('events.txt', 'sort_years.txt')
+processing_years.read_years()
+processing_minute.write()
+processing_hours.write()
+processing_month.write()
+processing_years.write()
 
 # После зачета первого этапа нужно сделать группировку событий
 #  - по часам
