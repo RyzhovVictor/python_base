c4682c4ddb6961d4f60450148ecf2af08620d3e1
Рыжов Виктор <ryzhov.dur@gmail.com>
2021-08-24 21:19:46 +0300

    Lessons_09 Task_02 Проверка второй части

--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -25,17 +25,16 @@
 
 
 class Events:
-    def __init__(self, input_file, output_file):
+    def __init__(self, input_file, output_file, group):
         self.input_file = input_file
         self.output_file = output_file
-        self.group = 0
+        self.group = group
         self.stat = {}
 
     def read_minute(self):
         with open(self.input_file, 'r', encoding='utf-8') as file:
             for line in file:
                 if 'NOK' in line:
-                    self.group = 17
                     line = line.strip()[1:self.group]
                     if line in self.stat:
                         self.stat[line] += 1
@@ -50,51 +49,26 @@ class Events:
 
 class Events_hours(Events):
     def read_hours(self):
-        with open(self.input_file, 'r', encoding='utf-8') as file:
-            for line in file:
-                if 'NOK' in line:
-                    self.group = 14  # TODO атрибут можно и нужно вынести в Init класса
-                    # TODO тогда весь этот метод дублировать будет не нужно, мы просто изменим в init одну строку
-                    line = line.strip()[1:self.group]
-                    if line in self.stat:
-                        self.stat[line] += 1
-                    else:
-                        self.stat[line] = 1
+        self.read_minute()
 
 
 class Events_month(Events):
     def read_month(self):
-        with open(self.input_file, 'r', encoding='utf-8') as file:
-            for line in file:
-                if 'NOK' in line:
-                    self.group = 8
-                    line = line.strip()[1:self.group]
-                    if line in self.stat:
-                        self.stat[line] += 1
-                    else:
-                        self.stat[line] = 1
+        self.read_minute()
 
 
 class Events_years(Events):
     def read_years(self):
-        with open(self.input_file, 'r', encoding='utf-8') as file:
-            for line in file:
-                if 'NOK' in line:
-                    self.group = 5
-                    line = line.strip()[1:self.group]
-                    if line in self.stat:
-                        self.stat[line] += 1
-                    else:
-                        self.stat[line] = 1
+        self.read_minute()
 
 
-processing_minute = Events('events.txt', 'sort_minute.txt')
+processing_minute = Events('events.txt', 'sort_minute.txt', 17)
 processing_minute.read_minute()
-processing_hours = Events_hours('events.txt', 'sort_hours.txt')
+processing_hours = Events_hours('events.txt', 'sort_hours.txt', 14)
 processing_hours.read_hours()
-processing_month = Events_month('events.txt', 'sort_month.txt')
+processing_month = Events_month('events.txt', 'sort_month.txt', 8)
 processing_month.read_month()
-processing_years = Events_years('events.txt', 'sort_years.txt')
+processing_years = Events_years('events.txt', 'sort_years.txt', 5)
 processing_years.read_years()
 
 
