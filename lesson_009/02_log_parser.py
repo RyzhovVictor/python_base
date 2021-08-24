d686c5af1088d0c2aef486244300acda064edac6
Рыжов Виктор <ryzhov.dur@gmail.com>
2021-08-24 18:50:43 +0300

    Lessons_09 Task_02 Проверка второй части

--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -28,45 +28,15 @@ class Events:
     def __init__(self, input_file, output_file):
         self.input_file = input_file
         self.output_file = output_file
+        self.group = 0
         self.stat = {}
 
     def read_minute(self):
         with open(self.input_file, 'r', encoding='utf-8') as file:
             for line in file:
                 if 'NOK' in line:
-                    line = line.strip()[1:17]
-                    if line in self.stat:
-                        self.stat[line] += 1
-                    else:
-                        self.stat[line] = 1
-
-    def read_hours(self):
-        with open(self.input_file, 'r', encoding='utf-8') as file:
-            for line in file:
-                if 'NOK' in line:
-                    line = line.strip()[1:14]  # TODO обратите внимание, что изменяется только одно число
-                    # TODO попробуйте сделать это число атрибутом класса (или параметром метода)
-                    # TODO и наследуйте новый класс с новым атрибутом (каждый класс будет иметь свой атрибут и делать свою группировку)
-                    if line in self.stat:
-                        self.stat[line] += 1
-                    else:
-                        self.stat[line] = 1
-
-    def read_month(self):
-        with open(self.input_file, 'r', encoding='utf-8') as file:
-            for line in file:
-                if 'NOK' in line:
-                    line = line.strip()[1:8]
-                    if line in self.stat:
-                        self.stat[line] += 1
-                    else:
-                        self.stat[line] = 1
-
-    def read_years(self):
-        with open(self.input_file, 'r', encoding='utf-8') as file:
-            for line in file:
-                if 'NOK' in line:
-                    line = line.strip()[1:5]
+                    self.group = 17
+                    line = line.strip()[1:self.group]
                     if line in self.stat:
                         self.stat[line] += 1
                     else:
@@ -78,14 +48,55 @@ class Events:
                 file.write(f'[{dates}] {count}\n')
 
 
+class Events_hours(Events):
+    def read_hours(self):
+        with open(self.input_file, 'r', encoding='utf-8') as file:
+            for line in file:
+                if 'NOK' in line:
+                    self.group = 14
+                    line = line.strip()[1:self.group]
+                    if line in self.stat:
+                        self.stat[line] += 1
+                    else:
+                        self.stat[line] = 1
+
+
+class Events_month(Events):
+    def read_month(self):
+        with open(self.input_file, 'r', encoding='utf-8') as file:
+            for line in file:
+                if 'NOK' in line:
+                    self.group = 8
+                    line = line.strip()[1:self.group]
+                    if line in self.stat:
+                        self.stat[line] += 1
+                    else:
+                        self.stat[line] = 1
+
+
+class Events_years(Events):
+    def read_years(self):
+        with open(self.input_file, 'r', encoding='utf-8') as file:
+            for line in file:
+                if 'NOK' in line:
+                    self.group = 5
+                    line = line.strip()[1:self.group]
+                    if line in self.stat:
+                        self.stat[line] += 1
+                    else:
+                        self.stat[line] = 1
+
+
 processing_minute = Events('events.txt', 'sort_minute.txt')
 processing_minute.read_minute()
-processing_hours = Events('events.txt', 'sort_hours.txt')
+processing_hours = Events_hours('events.txt', 'sort_hours.txt')
 processing_hours.read_hours()
-processing_month = Events('events.txt', 'sort_month.txt')
+processing_month = Events_month('events.txt', 'sort_month.txt')
 processing_month.read_month()
-processing_years = Events('events.txt', 'sort_years.txt')
+processing_years = Events_years('events.txt', 'sort_years.txt')
 processing_years.read_years()
+
+
 processing_minute.write()
 processing_hours.write()
 processing_month.write()
