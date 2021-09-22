156936ff15de6b248bb6f0b069a1a9a5939e10e5
Victor <ryzhov.dur@gmail.com>
2021-09-22 22:57:39 +0300

    Lessons_010 Task_01 Update work

--- lesson_010/01_fifth_element.py
+++ lesson_010/01_fifth_element.py
@@ -5,9 +5,16 @@
 BRUCE_WILLIS = 42
 
 input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
-leeloo = int(input_data[4])
-result = BRUCE_WILLIS * leeloo
-print(f'- Leeloo Dallas! Multi-pass № {result}!')
+try:
+    leeloo = int(input_data[4])
+    result = BRUCE_WILLIS * leeloo
+    print(f'- Leeloo Dallas! Multi-pass № {result}!')
+    if input_data > str(len(input_data[4])):
+        raise BaseException('Вы ввели больше 5 значений')
+except ValueError:
+    print('Нужно писать цифры')
+except IndexError:
+    print('Нужно ввести 5 цифр')
 
 # Ообернуть код и обработать исключительные ситуации для произвольных входных параметров
 # - ValueError - невозможно преобразовать к числу
