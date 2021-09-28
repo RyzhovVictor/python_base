8a128789b586c3d5a6f149bad4521213e03bf350
Victor <ryzhov.dur@gmail.com>
2021-09-28 14:50:56 +0300

    Lessons_010 Task_02 Update work

--- lesson_010/02_groundhog_day.py
+++ lesson_010/02_groundhog_day.py
@@ -16,9 +16,97 @@
 # кармы до уровня ENLIGHTENMENT_CARMA_LEVEL. Исключения обработать и записать в лог.
 # При создании собственных исключений максимально использовать функциональность
 # базовых встроенных исключений.
+import random
 
 ENLIGHTENMENT_CARMA_LEVEL = 777
 
-# TODO здесь ваш код
+
+def printed():
+    print(f'+{"+":-^50}+')
+
+
+excep = []
+
+
+def one_day():
+    file_log = f'| Сегодня я '
+    dice = random.randint(1, 13)
+    carma = random.randint(1, 7)
+    if dice == 1:
+        try:
+            raise BaseException('IamGodError')
+        except BaseException as exc:
+            printed()
+            print(f'{file_log}{exc.args}{"|":^47}')
+            printed()
+            excep.append(exc.args)
+    if dice == 2:
+        try:
+            raise BaseException('DrunkError')
+        except BaseException as exc:
+            printed()
+            print(f'{file_log}{exc.args}{"|":^49}')
+            printed()
+            excep.append(exc.args)
+    if dice == 3:
+        try:
+            raise BaseException('CarCrashError')
+        except BaseException as exc:
+            printed()
+            print(f'{file_log}{exc.args}{"|":^44}')
+            printed()
+            excep.append(exc.args)
+    if dice == 4:
+        try:
+            raise BaseException('GluttonyError')
+        except BaseException as exc:
+            printed()
+            print(f'{file_log}{exc.args}{"|":^44}')
+            printed()
+            excep.append(exc.args)
+    if dice == 5:
+        try:
+            raise BaseException('DepressionError')
+        except BaseException as exc:
+            printed()
+            print(f'{file_log}{exc.args}{"|":^40}')
+            printed()
+            excep.append(exc.args)
+    if dice == 6:
+        try:
+            raise BaseException('SuicideError')
+        except BaseException as exc:
+            printed()
+            print(f'{file_log}{exc.args}{"|":^45}')
+            printed()
+            excep.append(exc.args)
+    return carma
+
+
+total_carma = 0
+
+
+def parse_log():
+    count = 0
+    print(f'{"|":*<79}|')
+    print(f'{"|":<79}|')
+    print(f'|{"":>30}{"PARSE LOG EXCEPTION"}{"":<29}|')
+    print(f'{"|":<79}|')
+    print(f'{"|":*<79}|')
+    for line in excep:
+        count += 1
+        print(f'{"-":->80}')
+        print(f'Исключения {">":->15} {line}, {">":->15} счетчик - {count}')
+        print(f'{"-":->80}')
+
+
+while True:
+    print(f'{total_carma} - Ух! Карма растет!')
+    if total_carma <= ENLIGHTENMENT_CARMA_LEVEL:
+        total_carma += one_day()
+    else:
+        break
+
+parse_log()
 
 # https://goo.gl/JnsDqu
